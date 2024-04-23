1e846a38572f846703ae6e927b2da0f53f65a4ec
ElietePB2024 <lyluarth@gmail.com>
2024-04-23 07:20:41 -0300

    alteraçoes pedidas pelo professor

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const convertButton = document.querySelector(".convert-button");
 const currencySelect = document.querySelector(".currency-select");
 
 
+
 function convertValue() {
     const inputCurrencyValue = document.querySelector(".input-currency").value;
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert");
@@ -12,7 +13,7 @@ function convertValue() {
 console.log(currencySelect.value)
     const dolarToday = 5.20
     const euroToday = 6.20
-
+    const libraToday = 6.39
   
     if (currencySelect.value == "dolar"){
 
@@ -32,6 +33,16 @@ currencyValueConverted.innerHTML =  new Intl.NumberFormat("en-US", {
             }).format(inputCurrencyValue / euroToday)   
     }
 
+
+
+
+    
+    if (currencySelect.value == "libra"){
+        currencyValueConverted.innerHTML =  new Intl.NumberFormat("UK", {
+            style: "currency", 
+            currency: "GBP",
+            }).format(inputCurrencyValue / libraToday)   
+    }
     
     
     currencyValueToConvert.innerHTML =  new Intl.NumberFormat("pt-BR", {
@@ -55,7 +66,12 @@ if(currencySelect.value == "euro"){
 
 }
 
+if (currencySelect.value == "libra"){
+    currencyName.innerHTML = "Libra"
+    currencyImage.src = "assets/libra 1.png"
 
+
+}
 convertValue()
 
 }
